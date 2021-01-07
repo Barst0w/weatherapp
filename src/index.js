4468203f3de1a26a8d0f829fe8cf9034b565c31b
Barst0w <jakefmillward@gmail.com>
2021-01-07 13:35:04 +0000

    finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 /* eslint-disable no-console */
 const content = document.querySelector('#content');
 // Function that holds all the logic to retrieve data and append it to the main page
@@ -10,19 +11,24 @@ const checkWeather = async (cityInput) => {
         const locationElement = document.createElement('p');
         const tempElement = document.createElement('p');
         const weatherElement = document.createElement('p');
+        const divImg = document.createElement('img');
 
         div.className = ('dataDiv');
         locationElement.className = ('locationElement');
         tempElement.className = ('tempElement');
         weatherElement.className = ('weatherElement');
+        divImg.className = ('divImg');
 
         locationElement.textContent = `${city}, ${country}`;
         tempElement.textContent = `${temp}`;
         weatherElement.textContent = `${weather}`;
+        const imgPath = (weather.replace(/\s/g, ''));
+        divImg.src = (`/src/img/${imgPath}.png`);
 
         div.appendChild(locationElement);
         div.appendChild(tempElement);
         div.appendChild(weatherElement);
+        div.appendChild(divImg);
         content.appendChild(div);
     };
     // Grabs the data from log, and then runs the function above which appends the data to the DOM
@@ -31,7 +37,7 @@ const checkWeather = async (cityInput) => {
         const weather = (log.weather[0].description);
         const city = (log.name);
         const { country } = log.sys;
-        const newTemp = temp.toFixed(0);
+        const newTemp = `${temp.toFixed(0)}°C`;
         dataDiv(city, country, newTemp, weather);
     };
     checkData();
@@ -39,7 +45,23 @@ const checkWeather = async (cityInput) => {
 
 document.addEventListener('click', (e) => {
     const inputValue = document.querySelector('#inputBox').value;
+    if (e.target.matches('.dataDiv')) content.removeChild(e.target);
     if (e.target.matches('#inputBtn')) checkWeather(inputValue);
+    if (e.target.matches('#farenheightBtn')) {
+        const temp = document.getElementsByClassName('tempElement');
+        if (temp.item(0).innerHTML.includes('C')) {
+            for (let i = 0; i < temp.length; ++i) {
+                temp.item(i).innerHTML = `${(parseInt(temp.item(i).innerHTML, 10) * 1.8 + 32).toFixed(0)}°F`;
+            }
+        }
+    } if (e.target.matches('#celsiusBtn')) {
+        const temp = document.getElementsByClassName('tempElement');
+        if (temp.item(0).innerHTML.includes('F')) {
+            for (let i = 0; i < temp.length; ++i) {
+                temp.item(i).innerHTML = `${((parseInt(temp.item(i).innerHTML, 10) - 32) * 0.555).toFixed(0)}°C`;
+            }
+        }
+    }
 });
 
 // main > temp
